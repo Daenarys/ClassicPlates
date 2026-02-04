2fbf25a13ed5c470d4706e04223fe69893c6d5e5
Daenarys <androidwoz@gmail.com>
2026-02-04 13:50:43 +0100

    remove

--- ClassicPlates/config/defaults.lua
+++
@@ -1,9 +0,0 @@
-if ClassicPlates_Defaults == nil then ClassicPlates_Defaults = {} end
-
-ClassicPlates_Defaults.DefaultConfig = {
-    largerPlates = false
-}
-
-ClassicPlates_Defaults.Config = {
-    largerPlates
-}
