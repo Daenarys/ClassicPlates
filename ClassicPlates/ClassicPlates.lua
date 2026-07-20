aac8a4c68f0ef2ac5c8959cb62d65ed95f1006d3
Daenarys <androidwoz@gmail.com>
2026-07-20 22:14:27 +0200

    sync 9.x skin 12.0.7

--- ClassicPlates/ClassicPlates.lua
+++ ClassicPlates/ClassicPlates.lua
@@ -27,15 +27,6 @@ function Addon:UpdateVersion()
 	print("|cff33ff99Classic Plates|r: Updated to v" .. ClassicPlatesVersion)
 end
 
-CpNamePlateBorderTemplateMixin = {}
-
-function CpNamePlateBorderTemplateMixin:SetVertexColor(r, g, b, a)
-	a = a / self.numLayers
-	for i, texture in ipairs(self.Textures) do
-		texture:SetVertexColor(r, g, b, a)
-	end
-end
-
 hooksecurefunc(NamePlateAuraItemMixin, "SetAura", function(self)
 	if self:IsForbidden() then return end
 
@@ -249,21 +240,22 @@ local function SkinHealthBar(frame)
 	frame.healthBar.background:SetAllPoints(frame.healthBar)
 	frame.healthBar.background:SetColorTexture(0.2, 0.2, 0.2, 0.85)
 
-	frame.healthBar.border = CreateFrame("Frame", nil, frame.healthBar, "CpNamePlateFullBorderTemplate")
+	frame.healthBar.border = CreateFrame("Frame", nil, frame.healthBar, "NamePlateFullBorderTemplate")
+	frame.healthBar.border:UpdateSizes()
 
 	if isTarget then
-		frame.healthBar.border:SetVertexColor(1, 1, 1, 0.55)
+		frame.healthBar.border:SetVertexColor(1, 1, 1, 0.9)
 	else
-		frame.healthBar.border:SetVertexColor(0, 0, 0, 0.8)
+		frame.healthBar.border:SetVertexColor(0, 0, 0, 1)
 	end
 
 	hooksecurefunc(frame.healthBar, "UpdateSelectionBorder", function()
 		local isTarget = frame.healthBar:IsTarget()
 
 		if isTarget then
-			frame.healthBar.border:SetVertexColor(1, 1, 1, 0.55)
+			frame.healthBar.border:SetVertexColor(1, 1, 1, 0.9)
 		else
-			frame.healthBar.border:SetVertexColor(0, 0, 0, 0.8)
+			frame.healthBar.border:SetVertexColor(0, 0, 0, 1)
 		end
 	end)
 end
@@ -356,14 +348,9 @@ local function HandleNamePlateAdded(unit)
 end
 
 local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_LOGIN")
 f:RegisterEvent("NAME_PLATE_UNIT_ADDED")
 f:SetScript("OnEvent", function(self, event, unit)
-	if event == "PLAYER_LOGIN" then
-		if C_CVar.GetCVar("nameplateSelectedScale") ~= "1" then
-			C_CVar.SetCVar("nameplateSelectedScale", 1)
-		end
-	elseif event == "NAME_PLATE_UNIT_ADDED" then
+	if event == "NAME_PLATE_UNIT_ADDED" then
 		HandleNamePlateAdded(unit)
 	end
 end)
