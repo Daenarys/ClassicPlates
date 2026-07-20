f137d2e9821c7983d7e4b6e35e893cbab98be35d
Daenarys <androidwoz@gmail.com>
2026-07-20 11:07:27 +0200

    cleanup

--- ClassicPlates/ClassicPlates.lua
+++ ClassicPlates/ClassicPlates.lua
@@ -78,7 +78,7 @@ hooksecurefunc(NamePlateAurasMixin, "RefreshAuras", function(self)
 
 	local prevAura = nil
 	for _, aura in ipairs(debuffs) do
-		aura:SetScale(1.4)
+		aura:SetScale(1)
 		aura:ClearAllPoints()
 		if prevAura then
 			aura:SetPoint("TOPLEFT", prevAura, "TOPLEFT", 24, 0)
@@ -89,7 +89,7 @@ hooksecurefunc(NamePlateAurasMixin, "RefreshAuras", function(self)
 	end
 
 	for _, aura in ipairs(ccs) do
-		aura:SetScale(1.4)
+		aura:SetScale(1)
 		aura:ClearAllPoints()
 		if prevAura then
 			aura:SetPoint("TOPLEFT", prevAura, "TOPLEFT", 24, 0)
@@ -114,61 +114,6 @@ hooksecurefunc(NamePlateClassificationFrameMixin, "UpdateClassificationIndicator
 		else
 			self.classificationIndicator:SetAtlas(self.classificationAtlasElement)
 		end
-	end
-end)
-
-hooksecurefunc(NamePlateUnitFrameMixin, "UpdateAnchors", function(self)
-	if self:IsForbidden() then return end
-
-	self.castBar:ClearAllPoints()
-	self.ClassificationFrame:ClearAllPoints()
-	if ClassicPlatesDB.largerPlates then
-		self.castBar:SetHeight(22)
-		PixelUtil.SetPoint(self.castBar, "BOTTOMLEFT", self, "BOTTOMLEFT", 0, 0)
-		PixelUtil.SetPoint(self.castBar, "BOTTOMRIGHT", self, "BOTTOMRIGHT", 0, 0)
-		self.castBar.BorderShield:SetSize(16, 18)
-		self.castBar.Icon:SetSize(18, 18)
-		self.castBar.Text:SetTextHeight(16)
-		PixelUtil.SetHeight(self.HealthBarsContainer, 15)
-		self.name:SetFontObject("CpSystemFont_LargeNamePlate")
-		self.ClassificationFrame:SetPoint("RIGHT", self.HealthBarsContainer, "LEFT", -2, 0)
-	else
-		self.castBar:SetHeight(12)
-		PixelUtil.SetPoint(self.castBar, "BOTTOMLEFT", self, "BOTTOMLEFT", 28, 0)
-		PixelUtil.SetPoint(self.castBar, "BOTTOMRIGHT", self, "BOTTOMRIGHT", -28, 0)
-		self.castBar.BorderShield:SetSize(12, 14)
-		self.castBar.Icon:SetSize(14, 14)
-		self.castBar.Text:SetTextHeight(12)
-		PixelUtil.SetHeight(self.HealthBarsContainer, 5)
-		self.name:SetFontObject("CpSystemFont_NamePlate")
-		self.ClassificationFrame:SetPoint("RIGHT", self.HealthBarsContainer, "LEFT")
-	end
-	self.castBar.BorderShield:ClearAllPoints()
-	PixelUtil.SetPoint(self.castBar.BorderShield, "CENTER", self.castBar, "LEFT", 0, 0)
-	self.castBar.Icon:ClearAllPoints()
-	PixelUtil.SetPoint(self.castBar.Icon, "CENTER", self.castBar, "LEFT", 0, 0)
-	self.ClassificationFrame:SetScale(1.4)
-	self.ClassificationFrame:SetSize(14, 13)
-	self.ClassificationFrame.classificationIndicator:SetSize(14, 13)
-	self.HealthBarsContainer:ClearAllPoints()
-	PixelUtil.SetPoint(self.HealthBarsContainer, "BOTTOMLEFT", self.castBar, "TOPLEFT", 0, 2.5)
-	PixelUtil.SetPoint(self.HealthBarsContainer, "BOTTOMRIGHT", self.castBar, "TOPRIGHT", 0, 2.5)
-	self.name:SetIgnoreParentScale(true)
-	self.name:SetJustifyH("CENTER")
-	self.name:ClearAllPoints()
-	PixelUtil.SetPoint(self.name, "BOTTOM", self.HealthBarsContainer, "TOP", 0, 4)
-	if self.AurasFrame then
-		self.AurasFrame:SetSize(88, 14)
-		self.AurasFrame:ClearAllPoints()
-		self.AurasFrame:SetPoint("LEFT", self.HealthBarsContainer, "LEFT", -1, 0)
-		if self.HealthBarsContainer.healthBar:IsTarget() or self.name:IsShown() then
-			self.AurasFrame:SetPoint("BOTTOM", self, "TOP", 0, 8)
-		else
-			self.AurasFrame:SetPoint("BOTTOM", self.HealthBarsContainer, "TOP", 0, 10)
-		end
-	end
-	if self.AurasFrame.BuffListFrame then
-		self.AurasFrame.BuffListFrame:SetAlpha(0)
 	end
 end)
 
@@ -340,6 +285,60 @@ local function HandleNamePlateAdded(unit)
 		frame.selectionHighlight:SetBlendMode("ADD")
 		frame.selectionHighlight:SetAllPoints(frame.HealthBarsContainer)
 	end
+
+	hooksecurefunc(frame, "UpdateAnchors", function()
+		frame.castBar:ClearAllPoints()
+		frame.ClassificationFrame:ClearAllPoints()
+		if ClassicPlatesDB.largerPlates then
+			frame.castBar:SetHeight(22)
+			PixelUtil.SetPoint(frame.castBar, "BOTTOMLEFT", frame, "BOTTOMLEFT", 0, 0)
+			PixelUtil.SetPoint(frame.castBar, "BOTTOMRIGHT", frame, "BOTTOMRIGHT", 0, 0)
+			frame.castBar.BorderShield:SetSize(16, 18)
+			frame.castBar.Icon:SetSize(18, 18)
+			frame.castBar.Text:SetTextHeight(16)
+			PixelUtil.SetHeight(frame.HealthBarsContainer, 15)
+			frame.name:SetFontObject("CpSystemFont_LargeNamePlate")
+			frame.ClassificationFrame:SetPoint("RIGHT", frame.HealthBarsContainer, "LEFT", -2, 0)
+		else
+			frame.castBar:SetHeight(12)
+			PixelUtil.SetPoint(frame.castBar, "BOTTOMLEFT", frame, "BOTTOMLEFT", 28, 0)
+			PixelUtil.SetPoint(frame.castBar, "BOTTOMRIGHT", frame, "BOTTOMRIGHT", -28, 0)
+			frame.castBar.BorderShield:SetSize(12, 14)
+			frame.castBar.Icon:SetSize(14, 14)
+			frame.castBar.Text:SetTextHeight(12)
+			PixelUtil.SetHeight(frame.HealthBarsContainer, 5)
+			frame.name:SetFontObject("CpSystemFont_NamePlate")
+			frame.ClassificationFrame:SetPoint("RIGHT", frame.HealthBarsContainer, "LEFT")
+		end
+		frame.castBar.BorderShield:ClearAllPoints()
+		PixelUtil.SetPoint(frame.castBar.BorderShield, "CENTER", frame.castBar, "LEFT", 0, 0)
+		frame.castBar.Icon:ClearAllPoints()
+		PixelUtil.SetPoint(frame.castBar.Icon, "CENTER", frame.castBar, "LEFT", 0, 0)
+		frame.ClassificationFrame:SetScale(1.4)
+		frame.ClassificationFrame:SetSize(14, 13)
+		frame.ClassificationFrame.classificationIndicator:SetSize(14, 13)
+		frame.HealthBarsContainer:ClearAllPoints()
+		PixelUtil.SetPoint(frame.HealthBarsContainer, "BOTTOMLEFT", frame.castBar, "TOPLEFT", 0, 2.5)
+		PixelUtil.SetPoint(frame.HealthBarsContainer, "BOTTOMRIGHT", frame.castBar, "TOPRIGHT", 0, 2.5)
+		frame.name:SetIgnoreParentScale(true)
+		frame.name:SetJustifyH("CENTER")
+		frame.name:ClearAllPoints()
+		PixelUtil.SetPoint(frame.name, "BOTTOM", frame.HealthBarsContainer, "TOP", 0, 4)
+		if frame.AurasFrame then
+			frame.AurasFrame:SetIgnoreParentScale(true)
+			frame.AurasFrame:SetSize(88, 14)
+			frame.AurasFrame:ClearAllPoints()
+			frame.AurasFrame:SetPoint("LEFT", frame.HealthBarsContainer, "LEFT", -1, 0)
+			if frame.HealthBarsContainer.healthBar:IsTarget() or frame.name:IsShown() then
+				frame.AurasFrame:SetPoint("BOTTOM", frame, "TOP")
+			else
+				frame.AurasFrame:SetPoint("BOTTOM", frame.HealthBarsContainer, "TOP", 0, 5)
+			end
+		end
+		if frame.AurasFrame.BuffListFrame then
+			frame.AurasFrame.BuffListFrame:SetAlpha(0)
+		end
+	end)
 
 	frame.skinned = true
 end
